95886f17d0a4546925595309988220fa369cc27b
tccwilkinson <wilkinson@ucsb.edu>
2026-07-29 09:19:02 +0900

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   server: {
     port: 3001,
     strictPort: false,
